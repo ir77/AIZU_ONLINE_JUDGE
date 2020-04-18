f9346070f8305def9a7a84c9e555381653d419ce
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2020-04-18 12:33:22 +0900

    add

---
+++ 1ITP/1_1_B/1_1_B.rs
@@ -0,0 +1,13 @@
+use std::io;
+
+fn main() {
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse()
+        .expect("Failed to parse");
+        
+    println!("{}", guess * guess * guess);
+}
