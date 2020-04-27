cf764c344467504f8307c470917a5986a6a54cde
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2020-04-27 21:20:53 +0900

    add

---
+++ 1ITP/1_2_B/1_2_B.rs
@@ -0,0 +1,50 @@
+use std::io;
+use std::cmp::Ordering;
+
+fn main() {
+    let (input1, input2, input3) = input_3_number();
+
+    match input1.cmp(&input2) {
+        Ordering::Greater => println!("No"),
+        Ordering::Equal => println!("No"),
+        Ordering::Less => {
+            match input2.cmp(&input3) {
+                Ordering::Greater => println!("No"),
+                Ordering::Equal => println!("No"),
+                Ordering::Less => {
+                    println!("Yes");
+                    return;
+                }        
+            }
+        },
+    }
+}
+
+#[allow(dead_code)]
+fn input_3_number() -> (i32, i32, i32) {
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input)
+        .expect("Failed to read line");
+
+    let vec: Vec<&str> = input.split_whitespace().collect();
+
+    let input1: i32 = vec[0].parse().unwrap_or(0);
+    let input2: i32 = vec[1].parse().unwrap_or(0);
+    let input3: i32 = vec[2].parse().unwrap_or(0);
+
+    (input1, input2, input3)
+}
+
+#[allow(dead_code)]
+fn input() -> i32 {
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input)
+        .expect("Failed to read line");
+    let input: i32 = input.trim().parse()
+        .expect("Failed to parse");
+
+    input
+}
+
