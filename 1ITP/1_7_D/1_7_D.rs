88082ccdb7813d952f6277417cf8af2ab68a3b7f
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2020-05-06 16:25:56 +0900

    add

---
+++ 1ITP/1_7_D/1_7_D.rs
@@ -0,0 +1,45 @@
+use std::io;
+
+fn main() {
+    let input = input_number();
+    let n = input[0] as usize;
+    let m = input[1] as usize;
+    let l = input[2] as usize;
+    let mut nm: Vec<Vec<i64>> = vec![];
+    for _ in 0..n {
+        nm.push(input_number());
+    }
+    let mut ml: Vec<Vec<i64>> = vec![];
+    for _ in 0..m {
+        ml.push(input_number());
+    }
+    for i in 0..n {
+        for j in 0..l {
+            let sum = (0..m).fold(0, |sum, k| sum + nm[i][k] * ml[k][j]);
+            print!("{}{}", sum, if j == l-1 { '\n' } else { ' ' });
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn input_number() -> Vec<i64> {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    let vec: Vec<i64> = input
+        .split_whitespace()
+        .map(|x| x.parse().unwrap_or(0))
+        .collect();
+    vec
+}
+
+#[allow(dead_code)]
+fn input() -> (char, usize) {
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).expect("");
+    let mut iter = s.trim().split_whitespace();
+    let c: char = iter.next().unwrap().chars().nth(0).unwrap();
+    let n: usize = iter.next().unwrap().parse().unwrap();
+    (c, n)
+}
